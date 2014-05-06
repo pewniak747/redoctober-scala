66de75e6e74774282d14a0ec2534786d6195cb84
Tomasz Pewiński <pewniak747@gmail.com>
2014-05-06 14:39:51 +0200

    Initial commit

---
+++ src/main/scala/Runner.scala
@@ -0,0 +1,11 @@
+package redoctober
+
+import akka.actor._
+
+object Runner {
+  def main(args: Array[String]) = {
+    println("Starting RED October Simulation...")
+
+    val actorSystem = ActorSystem()
+  }
+}
